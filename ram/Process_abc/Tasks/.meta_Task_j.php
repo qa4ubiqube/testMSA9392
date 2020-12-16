b2deedf754921c3f9f6094400249d1a65420b6ff
jboss <jboss@c1349aab8cdc>
2020-12-16 15:50:26 +0000

    16-12-2020 15:50:26 - Updating BPMN on  - ncroot

---
+++ ram/Process_abc/Tasks/.meta_Task_j.php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<metadata>
+    <map>
+        <entry>
+            <key>DISPLAYNAME</key>
+            <value>Task_j.php</value>
+        </entry>
+        <entry>
+            <key>DATE_MODIFICATION</key>
+            <value>1608133826273</value>
+        </entry>
+        <entry>
+            <key>REPOSITORY</key>
+            <value>Process</value>
+        </entry>
+        <entry>
+            <key>DATE_CREATION</key>
+            <value>1608133826268</value>
+        </entry>
+        <entry>
+            <key>TAG</key>
+        </entry>
+        <entry>
+            <key>FILE_TYPE</key>
+            <value>text</value>
+        </entry>
+        <entry>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
+        </entry>
+        <entry>
+            <key>COMMENT</key>
+        </entry>
+    </map>
+</metadata>
